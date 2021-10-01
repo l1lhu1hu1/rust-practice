9a0dd29c6dd90c2652f875598fc9b8aa1b0f5064
Koki Tsumura <koki827tt@gmail.com>
2021-10-01 22:03:38 +0900

    splitted a module into a file

--- rust-lang-doc/defining-modules-to-control-scope-and-privacy/src/lib.rs
+++ rust-lang-doc/defining-modules-to-control-scope-and-privacy/src/lib.rs
@@ -1,21 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
+mod front_of_house;
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-// 以下の書き方でも可能
-// use crate::front_of_house::hosting;
-use self::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 fn serve_order() {}
 
