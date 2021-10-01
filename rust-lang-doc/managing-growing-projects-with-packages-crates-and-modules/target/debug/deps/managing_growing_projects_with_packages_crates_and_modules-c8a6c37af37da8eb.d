655ee944d9ce2ac43c636a419df69a5262abf599
Koki Tsumura <koki827tt@gmail.com>
2021-10-01 22:09:35 +0900

    renamed defining modules to control scope and privacy to managing growing projects with packages crates and modules

---
+++ rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules-c8a6c37af37da8eb.d
@@ -0,0 +1,8 @@
+/Users/glutton/projects/rust-practice/rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules-c8a6c37af37da8eb.rmeta: src/lib.rs src/front_of_house.rs
+
+/Users/glutton/projects/rust-practice/rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/libmanaging_growing_projects_with_packages_crates_and_modules-c8a6c37af37da8eb.rlib: src/lib.rs src/front_of_house.rs
+
+/Users/glutton/projects/rust-practice/rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules-c8a6c37af37da8eb.d: src/lib.rs src/front_of_house.rs
+
+src/lib.rs:
+src/front_of_house.rs:
---
+++ rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules.d
@@ -0,0 +1,5 @@
+/Users/glutton/projects/rust-practice/rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules: src/main.rs
+
+/Users/glutton/projects/rust-practice/rust-lang-doc/managing-growing-projects-with-packages-crates-and-modules/target/debug/deps/managing_growing_projects_with_packages_crates_and_modules.d: src/main.rs
+
+src/main.rs:
