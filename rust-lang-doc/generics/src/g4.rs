843908a181035baaf941a811b3be16c9692e2535
Koki Tsumura <koki827tt@gmail.com>
2021-10-05 18:27:05 +0900

    added generics related code

--- rust-lang-doc/generics/src/g4.rs
+++ rust-lang-doc/generics/src/g4.rs
@@ -1,19 +1,38 @@
 struct A;
+// ()でstructを表す場合は、型のみを書く
+// {}でstructを表す場合は、フィールドと型を書く
+
 // Aという型のみ受け付けられる
 struct Single(A);
 
 // どんな型でも受け付けられる
 struct SingleGen<T>(T);
 
+struct S(A);
+struct SGen<T>(T);
+
 // ワンラインで宣言したstructのインスタンスを、作ったときには、
 // フィールドにアクセスする時に、tp.0, tp.1のようにしないといけない
-struct Tuple(u32, String);
+struct Tuple(i32, String);
 struct Point(i32);
 
 struct Hello<T> {
     ja: T,
     en: T,
 }
+
+fn reg_fn(_s: S) {}
+
+// ジェネリックではない
+// 明示的な型をパラーメーターとしてi32を与えられているから
+// gen_spec_tの直後にもし<A>があれば、ジェネリックな関数になる
+fn gen_spec_t(_s: SGen<A>) {}
+
+// ジェネリックではない
+// 明示的な型をパラーメーターとしてi32を与えられているから
+fn gen_spec_i32(_s: SGen<i32>) {}
+
+fn generic<T>(_s: SGen<T>) {}
 
 fn main() {
     let _s = Single(A);
@@ -35,4 +54,14 @@ fn main() {
     };
 
     println!("ja: {}, en: {}", h.ja, h.en);
+
+    println!("########################################");
+
+    reg_fn(S(A));
+    gen_spec_t(SGen(A));
+    gen_spec_i32(SGen(6));
+
+    // どちらでもオッケー
+    generic::<char>(SGen('a'));
+    generic(SGen('c'));
 }
---
+++ rust-lang-doc/generics/src/g8.rs
@@ -0,0 +1,28 @@
+struct Subject<T> {
+    eng: T,
+    math: T,
+}
+
+trait Random<T> {
+    fn bye() {
+        println!("bye");
+    }
+    fn hello(t: T) -> T {
+        t
+    }
+    fn ttt(&self) {}
+}
+
+impl<T, U> Random<T> for Subject<U> {
+    fn ttt(&self) {
+        println!("ttttt");
+    }
+}
+
+fn main() {
+    let s = Subject {
+        math: "32",
+        eng: "55",
+    };
+    // s.ttt();
+}
