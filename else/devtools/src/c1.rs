de2fecdb076651ac7e6c9cf3ce436e06f42d32b6
Koki Tsumura <koki827tt@gmail.com>
2021-10-21 09:41:08 +0900

    added example code for trying clippy and other rust devtools

---
+++ else/devtools/src/c1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let x = 3.1415;
+    let x = std::f32::consts::PI;
+    // let x = 3.1415;
+    let r = 8.0;
+    let _a = 20;
+    let _b = 40;
+    println!("the area of the circle is {}", r * r * x);
+}
---
+++ else/devtools/src/main.rs
@@ -0,0 +1,6 @@
+fn do_something() {}
+fn main() {
+    for _i in 0..100 {
+        do_something();
+    }
+}
